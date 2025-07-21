cb24aa127940150c32472750736ae42482e01752
Himanshu Agarkar <agarkarhimanshu456@gmail.com>
2025-07-21 18:10:40 +0530

    feature1 implemented in 6 and 9

--- 006_number_pyramid_pattern.cpp
+++ 006_number_pyramid_pattern.cpp
@@ -10,6 +10,24 @@ void printNumberPyramid(int n) {
   }
 
 int main() {
+
+
+void printNumberPyramid(int n) {
+    for (int row = 1; row <= n; row++) {
+        for(int col=1; col<=n-row; col++) cout << " ";
+        for(int col=1; col<=row; col++) cout << col;
+        cout << endl;
+    }
+  }
+
+
+void printNumberPyramid(int n) {
+    for (int row = 1; row <= n; row++) {
+        for(int col=1; col<=n-row; col++) cout << " ";
+        for(int col=1; col<=row; col++) cout << col;
+        cout << endl;
+    }
+  }
     printNumberPyramid(4);
     return 0;
 }
