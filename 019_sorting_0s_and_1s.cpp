b38d15cb46c6e0f3f3caa4f424efd73e93ad0755
Himanshu Agarkar <agarkarhimanshu456@gmail.com>
2025-07-21 18:16:37 +0530

    feature1 file 19

--- 019_sorting_0s_and_1s.cpp
+++ 019_sorting_0s_and_1s.cpp
@@ -5,18 +5,6 @@ using namespace std;
 void sorting0sAnd1s() {
 
 
-    int left = 0, right = v.size()-1;
-
-    while(left < right) {
-        if(v[left] == 0) left++;
-        else if(v[right] == 1) right--;
-        else {
-            swap(v[left], v[right]);
-            left++;
-            right--;
-        }
-    }
-
     for(int i=0; i<v.size(); i++) cout << v[i] << " ";
 }
 
